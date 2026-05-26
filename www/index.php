b2698890d997ade5c569bb79c3af774e5e845202
PUPOLIK <yaroslav.pupolik@gmail.com>
2026-05-26 12:44:41 +0300

    add css in index.php

--- www/index.php
+++ www/index.php
@@ -3,11 +3,11 @@
 <html>
  <head>
   <title>Hello... PostgreSQL</title>
-
+  
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
@@ -53,4 +53,4 @@
     ?>
     </div>
 </body>
-</html>
+</html>
